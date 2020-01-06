0a5112763d8e65ab11891253328f054501594b9f
imperfect <wenghaolula@outlook.com>
2020-01-06 08:44:34 +0800

    singer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React,{Component} from 'react'
 import {Route,Switch,Redirect} from 'react-router-dom'
-import Login from './containers/home/home.jsx' 
-import Singer from './containers/singer/singer.jsx' 
 import Index from './containers/index/index.jsx' 
 
 
@@ -10,9 +8,6 @@ export default class App extends Component{
     return (
       <div className="app">
         <Switch>
-          <Route path="/home" component={Login}/>
-          <Route path="/singer" component={Singer}/>
-          <Redirect to='/home'/>
           <Route path="/index" component={Index}/>
           <Redirect to='/index/music'/>
         </Switch>
