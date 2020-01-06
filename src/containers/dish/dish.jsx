d4f77ccfbb171c22c9cd5fe8c5308bf4be02408a
solidMan <1853324556@qq.com>
2020-01-06 11:50:00 +0800

    1111

--- src/containers/dish/dish.jsx
+++ src/containers/dish/dish.jsx
@@ -1,9 +1,228 @@
 import React,{Component} from 'react'
+import './css/index.less'
 
 export default class MyComponent extends Component{
+  state = {
+    playList:[
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      {
+        pic:"https://y.gtimg.cn/music/photo_new/T002R300x300M0000008Q19i2O1LZ0_1.jpg?max_age=2592000",
+        album:"小寒",
+        singer:"赵芳婧",
+        sing:"音阙诗听",
+        data:"2020-01-06"
+      },
+      
+    ]
+  }
   render(){
     return (
-      <div>dish</div>
+      <div className="dish_container">
+        <div className="top"></div>
+        <header className="nav">
+          <ul className="tagList">
+            <li className="item">内地</li>
+            <li className="item">港台</li>
+            <li className="item">欧美</li>
+            <li className="item">韩国</li>
+            <li className="item">日本</li>
+            <li className="item">其他</li>
+          </ul>
+        </header>
+        <div className="play_all">
+          <ul className="play_list">
+          {
+                    this.state.playList.splice(0,20).map((playItem,index)=>{
+                      return (
+                        <li key={index} className="play_item">
+                          <div className="pic">
+                            <img src={playItem.pic} alt=""/>
+                          </div>
+                          <div className="text">
+                            <h4 className="album">
+                              <span>{playItem.album}</span>
+                            </h4>
+                            <div className="singer">
+                              <span>{playItem.sing}</span>
+                              <span>/</span>
+                              <span>{playItem.singer}</span>
+                            </div>
+                            <span className="date">{playItem.data}</span>
+                          </div>
+                        </li>
+                      )
+                    })
+                  }
+            </ul>
+        </div>
+      </div>
     )
   }
 }
